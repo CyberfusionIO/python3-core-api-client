10da15bd7fc8168b87d4f071decce4b52f845384
William David Edwards <wedwards@cyberfusion.nl>
2025-07-06 13:28:07 +0200

    Add `RootModelCollectionMixin`
    As `BaseModel.__iter__`'s return type is `TupleGenerator` (for reasons unknown to man; see https://github.com/pydantic/pydantic/discussions/5282#discussioncomment-7866922), we need to ignore the deviating return type in our mixin.
    
    To then prevent mypy inside pre-commit from complaining about `unused-ignore`, Pydantic must be present within that environment. Unfortunately, this requires duplicating the dependency (see https://github.com/pre-commit/pre-commit/issues/730).

---
+++ tests/unit_tests/test_models.py
@@ -0,0 +1,98 @@
+from pydantic import BaseModel
+
+from cyberfusion.CoreApiClient.models import RootModelCollectionMixin
+import faker
+import pytest
+
+
+def test_RootModelCollectionMixin_iter_not_dict_or_list(faker: faker.Faker) -> None:
+    class Model(RootModelCollectionMixin, BaseModel):
+        __root__: int
+
+    model = Model(__root__=faker.pyint())
+
+    with pytest.raises(TypeError, match="^Type does not support iter$"):
+        for _ in model:
+            pass
+
+
+def test_RootModelCollectionMixin_getitem_not_dict_or_list(faker: faker.Faker) -> None:
+    class Model(RootModelCollectionMixin, BaseModel):
+        __root__: int
+
+    model = Model(__root__=faker.pyint())
+
+    with pytest.raises(TypeError, match="^Type does not support getitem$"):
+        model["a"]
+
+
+def test_RootModelCollectionMixin_items_not_dict(faker: faker.Faker) -> None:
+    class Model(RootModelCollectionMixin, BaseModel):
+        __root__: int
+
+    model = Model(__root__=faker.pyint())
+
+    with pytest.raises(TypeError, match="^Type does not support items$"):
+        model.items()
+
+
+def test_RootModelCollectionMixin_dict_iter(faker: faker.Faker) -> None:
+    dict_ = {"a": "b"}
+
+    class Model(RootModelCollectionMixin, BaseModel):
+        __root__: dict[str, str]
+
+    model = Model(__root__=dict_)
+
+    for k in model:
+        assert k == "a"
+
+    for k, v in model.items():
+        assert k == "a"
+        assert v == "b"
+
+
+def test_RootModelCollectionMixin_dict_items(faker: faker.Faker) -> None:
+    dict_ = {"a": "b"}
+
+    class Model(RootModelCollectionMixin, BaseModel):
+        __root__: dict[str, str]
+
+    model = Model(__root__=dict_)
+
+    items = model.items()
+
+    assert list(items) == [("a", "b")]
+
+
+def test_RootModelCollectionMixin_list_iter(faker: faker.Faker) -> None:
+    list_ = ["a", "b"]
+
+    class Model(RootModelCollectionMixin, BaseModel):
+        __root__: list[str]
+
+    model = Model(__root__=list_)
+
+    assert [v for v in model] == list_
+
+
+def test_RootModelCollectionMixin_list_getitem(faker: faker.Faker) -> None:
+    list_ = ["a"]
+
+    class Model(RootModelCollectionMixin, BaseModel):
+        __root__: list[str]
+
+    model = Model(__root__=list_)
+
+    assert model[0] == "a"
+
+
+def test_RootModelCollectionMixin_dict_getitem(faker: faker.Faker) -> None:
+    dict_ = {"a": "b"}
+
+    class Model(RootModelCollectionMixin, BaseModel):
+        __root__: dict[str, str]
+
+    model = Model(__root__=dict_)
+
+    assert model["a"] == "b"
